386f5f7fdd91884da918897b5f80491992c4bbe5
matheus.gdoliveira7@gmail.com <matheus.gdoliveira7@gmail.com>
2025-07-30 17:28:04 -0300

    realizando a conexão com o banco de dados.

---
+++ CrudProdutos/Models/ProdutosModel.cs
@@ -0,0 +1,16 @@
+﻿namespace CrudProdutos.Models
+{
+    public class ProdutosModel
+    {
+        public int Id { get; set; } // Referênciar uma Coluna da Tabela
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Preco { get; set; }
+
+        public string Categoria { get; set; }
+
+    }
+}
