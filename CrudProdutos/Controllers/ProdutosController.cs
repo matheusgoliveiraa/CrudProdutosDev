4b7af592dfba17969604a384336c5304b3c5ce56
matheus.gdoliveira7@gmail.com <matheus.gdoliveira7@gmail.com>
2025-07-30 21:33:31 -0300

    Adicionando as páginas, Cadastrar e Editar...

--- CrudProdutos/Controllers/ProdutosController.cs
+++ CrudProdutos/Controllers/ProdutosController.cs
@@ -13,11 +13,37 @@ namespace CrudProdutos.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index() // Indo no banco e buscando todos os produtos do banco
         {
             IEnumerable<ProdutosModel> produtos = _db.Produtos; // Pegando a tabela completa
-            
-            return View(produtos);
+            return View(produtos); // Retornando Tabela do Banco
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(ProdutosModel produtos)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Produtos.Add(produtos);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
         }
     }
 }
--- CrudProdutos/Models/ProdutosModel.cs
+++ CrudProdutos/Models/ProdutosModel.cs
@@ -1,15 +1,21 @@
-﻿namespace CrudProdutos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudProdutos.Models
 {
     public class ProdutosModel
     {
         public int Id { get; set; } // Referênciar uma Coluna da Tabela
 
+        [Required(ErrorMessage = "Digite o nome do produto!")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Digite a descrição do produto!")]
         public string Descricao { get; set; }
 
+        [Required(ErrorMessage = "Digite o preço do produto!")]
         public string Preco { get; set; }
 
+        [Required(ErrorMessage = "Digite a categoria do produto!")]
         public string Categoria { get; set; }
 
     }
